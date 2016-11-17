7b82c78473432fe5336e11da8c9992f59e79a4b7
Gerhard Herbert <gerhard.herbert@avira.com>
2016-11-17 23:27:35 +0100

    Fixed all tests

--- fsharp/prime-factors/PrimeFactors.fs
+++ fsharp/prime-factors/PrimeFactors.fs
@@ -1,19 +1,13 @@
 ﻿module PrimeFactors
 open System
 
-let rec getFactor (divisor:Int64) (n:Int64, factors:Int64 list) =
-    match (n, factors) with
+let rec applyDivisor (divisor:Int64) (number:Int64, factors:Int64 list) =
+    match (number, factors) with
     | (1L, factors) -> (1L, factors)
-    | (n, factors)  when n % divisor <> 0L -> (n, factors)
-    | (n, factors)  -> getFactor divisor (n / divisor, List.append factors  [divisor])
+    | (n, factors)  when number % divisor <> 0L -> (number, factors)
+    | (n, factors)  -> applyDivisor divisor (n / divisor, List.append factors  [divisor])
 
 let primeFactorsFor (number:Int64) =
-    seq {for i = 2L to number/2L do yield i; yield number}
-    |> Seq.fold (fun (n, factors) d -> getFactor d (n, factors)) (number, [])
+    Seq.append { 2L .. number/2L} {number..number}
+    |> Seq.fold (fun (n, factors) divisor -> applyDivisor divisor (n, factors)) (number, [])
     |> snd
-
-let x = primeFactorsFor (2L)
-
-let number = 10L
-let s = seq {for i = 2L to number/2L do yield i}
-s
--- fsharp/prime-factors/PrimeFactorsTest.fs
+++ fsharp/prime-factors/PrimeFactorsTest.fs
@@ -13,41 +13,33 @@ let ``Test 2`` () =
     Assert.That(primeFactorsFor 2L, Is.EqualTo([2L]))
     
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Test 3`` () =
     Assert.That(primeFactorsFor 3L, Is.EqualTo([3L]))
 
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Test 4`` () =
     Assert.That(primeFactorsFor 4L, Is.EqualTo([2L; 2L]))
     
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Test 6`` () =
     Assert.That(primeFactorsFor 6L, Is.EqualTo([2L; 3L]))
     
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Test 8`` () =
     Assert.That(primeFactorsFor 8L, Is.EqualTo([2L; 2L; 2L]))
     
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Test 9`` () =
     Assert.That(primeFactorsFor 9L, Is.EqualTo([3L; 3L]))
     
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Test 27`` () =
     Assert.That(primeFactorsFor 27L, Is.EqualTo([3L; 3L; 3L]))
     
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Test 625`` () =
     Assert.That(primeFactorsFor 625L, Is.EqualTo([5L; 5L; 5L; 5L]))
     
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Test 901255`` () =
     Assert.That(primeFactorsFor 901255L, Is.EqualTo([5L; 17L; 23L; 461L]))
