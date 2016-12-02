9286dae3fb6997939376bb2fc8502f01b9016678
Gerhard Herbert <gerhard.herbert@avira.com>
2016-12-02 20:07:44 +0100

    Some refactoring for Bob

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -2,30 +2,22 @@
 
 open System
 
-let isUppcase (msg : string) = 
-    msg.ToCharArray() 
-    |> Seq.fold (fun r c -> r && (Char.IsUpper(c) || Char.IsLetter(c) <> true) ) true
+let checkAll (msg : string) condition = 
+    msg.ToCharArray()
+    |> Seq.fold (fun r c -> r && (condition c)) true
 
-let noLetter (msg : string) = 
-    msg.ToCharArray() 
-    |> Seq.fold (fun r c -> r &&  Char.IsLetter(c) = false) true
-
-let isWhiteSpace (msg : string) = 
-    msg.ToCharArray() 
-    |> Seq.fold (fun r c -> r &&  Char.IsWhiteSpace(c)) true
-
-let endsWith prefix (msg:string) =
-    msg.EndsWith(prefix) 
-
-let isShouting msg =
-    isUppcase msg && ((endsWith "?" msg) || (endsWith "!" msg)) 
+let isAllUppercase msg = checkAll msg (fun c -> Char.IsUpper c  || not (Char.IsLetter c))
+let noLetter msg = checkAll msg (fun c -> Char.IsLetter c |> not)
+let atLeastOneLetter msg = noLetter msg |> not
+let isYelling msg = isAllUppercase msg && atLeastOneLetter msg 
+let endsWith prefix (msg:string) = msg.EndsWith(prefix) 
+let isQuestion msg =  endsWith "?" msg
+let isEmpty msg = checkAll msg (fun c -> Char.IsWhiteSpace c)
 
 let hey msg = 
     match msg with
-    | _ when isWhiteSpace  msg ->  "Fine. Be that way!"
-    | _ when isUppcase msg = false && endsWith "?" msg ->  "Sure."
-    | _ when noLetter msg && endsWith "?" msg ->  "Sure."
-    | _ when noLetter msg ->  "Whatever."
-    | _ when isUppcase msg ->  "Whoa, chill out!"
+    | _ when isEmpty  msg ->  "Fine. Be that way!"
+    | _ when isYelling msg ->  "Whoa, chill out!"
+    | _ when isQuestion msg ->  "Sure."
     | _ ->  "Whatever."
 
